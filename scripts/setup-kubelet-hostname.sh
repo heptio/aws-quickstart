bfd0f3602875ba0ea6f163f51e4dcea669f086a2
Chuck Ha <chuck@heptio.com>
2018-01-11 14:09:14 -0500

    Minor update to kubelet configuration
    * Adds cloud provider argument
    * Do not restart kubelet before kubadm runs
    
    Signed-off-by: Chuck Ha <chuck@heptio.com>

--- scripts/setup-kubelet-hostname.sh
+++ scripts/setup-kubelet-hostname.sh
@@ -4,7 +4,6 @@ HOSTNAME="$(hostname -f)"
 
 /bin/cat > /etc/systemd/system/kubelet.service.d/10-hostname.conf <<EOF
 [Service]
-Environment="KUBELET_EXTRA_ARGS= --hostname-override=${HOSTNAME}"
+Environment="KUBELET_EXTRA_ARGS= --hostname-override=${HOSTNAME} --cloud-provider=aws"
 EOF
 systemctl daemon-reload
-systemctl restart kubelet
