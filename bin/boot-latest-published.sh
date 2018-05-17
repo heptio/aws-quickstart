6d563d3c98cfbc91e850e3abc7c69ef906baec09
Chuck Ha <chuck@heptio.com>
2018-05-17 16:33:53 -0400

    Documentation update
    * Moves code from README into the bin directory.
    * Adds a troubleshooting section.
    
    Closes #182
    
    Signed-off-by: Chuck Ha <chuck@heptio.com>

---
+++ bin/boot-latest-published.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Where to place your cluster
+REGION=us-west-2
+AZ=us-west-2b
+
+# What to name your CloudFormation stack
+STACK=Heptio-Kubernetes
+
+# Which SSH key you want to allow access to the cluster
+KEYNAME=laptop
+
+# Import an SSH public key (or skip this command and create/import an SSH key pair in the AWS console)
+aws ec2 --region $REGION import-key-pair --key-name $KEYNAME --public-key-material "$(cat ~/.ssh/id_rsa.pub)"
+
+# What IP addresses should be able to connect over SSH and over the Kubernetes API
+INGRESS=0.0.0.0/0
+
+aws cloudformation create-stack \
+  --region $REGION \
+  --stack-name $STACK \
+  --template-url "https://s3.amazonaws.com/quickstart-reference/heptio/latest/templates/kubernetes-cluster-with-new-vpc.template" \
+  --parameters \
+    ParameterKey=AvailabilityZone,ParameterValue=$AZ \
+    ParameterKey=KeyName,ParameterValue=$KEYNAME \
+    ParameterKey=AdminIngressLocation,ParameterValue=$INGRESS \
+  --capabilities=CAPABILITY_IAM
---
+++ bin/boot-master-branch.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+# This is where Heptio stores templates/scripts for the master branch of this repository
+S3_BUCKET=heptio-aws-quickstart-test
+S3_PREFIX=heptio/kubernetes/master/
+
+# Where to place your cluster
+REGION=us-west-2
+AZ=us-west-2b
+
+# What to name your CloudFormation stack
+STACK=Heptio-Kubernetes
+
+# What SSH key you want to allow access to the cluster (must be created ahead of time in your AWS EC2 account)
+KEYNAME=mykey
+
+# What IP addresses should be able to connect over SSH and over the Kubernetes API
+INGRESS=0.0.0.0/0
+
+aws cloudformation create-stack \
+  --region $REGION \
+  --stack-name $STACK \
+  --template-url "https://${S3_BUCKET}.s3.amazonaws.com/${S3_PREFIX}templates/kubernetes-cluster-with-new-vpc.template" \
+  --parameters \
+    ParameterKey=AvailabilityZone,ParameterValue=$AZ \
+    ParameterKey=KeyName,ParameterValue=$KEYNAME \
+    ParameterKey=AdminIngressLocation,ParameterValue=$INGRESS \
+    ParameterKey=QSS3BucketName,ParameterValue=${S3_BUCKET} \
+    ParameterKey=QSS3KeyPrefix,ParameterValue=${S3_PREFIX} \
+  --capabilities=CAPABILITY_IAM
